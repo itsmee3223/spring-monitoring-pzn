2f08ce321f3730f5d770bdee648b1a027516c593
itsmee3223 <ramandaajisakaa@gmail.com>
2024-06-11 16:41:35 +0700

    create metrics

--- src/main/java/belajar/spring/monitoring/MyScheduleTask.java
+++ src/main/java/belajar/spring/monitoring/MyScheduleTask.java
@@ -1,14 +1,21 @@
 package belajar.spring.monitoring;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class MyScheduleTask {
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     @Scheduled(fixedDelay = 10000)
     public void hello(){
+        meterRegistry.counter("my.scheduled.task").increment();
         log.info("Hello world");
     }
 }
